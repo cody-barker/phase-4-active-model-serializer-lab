89846d179a38967170dbc76e217afed85268721d
Cody Barker <codybarker.or@gmail.com>
2023-06-15 11:14:24 -0700

    s

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,8 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :short_content
+  attributes :title, :content
+  belongs_to :author
   has_many :tags
-
-  def short_content
-    "#{self.object.content[0..49]}..."
-  end
 end
